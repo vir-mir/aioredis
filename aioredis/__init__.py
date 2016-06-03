921180b5f3f916a0bbe3b5216f5b158c725584d4
Alexey Popravka <alexey.popravka@horsedevel.com>
2016-06-03 17:36:42 +0300

    get rid of connection usage in Redis

--- aioredis/__init__.py
+++ aioredis/__init__.py
@@ -1,6 +1,6 @@
 from .connection import RedisConnection, create_connection
 from .commands import Redis, create_redis, create_reconnecting_redis
-from .pool import RedisPool, create_pool
+from .pool import ConnectionsPool, create_pool
 from .pubsub import Channel
 from .errors import (
     ConnectionClosedError,
@@ -17,10 +17,12 @@ from .errors import (
 
 __version__ = '0.2.9'
 
+RedisPool = ConnectionsPool
+
 # make pyflakes happy
 (create_connection, RedisConnection,
  create_redis, create_reconnecting_redis, Redis,
- create_pool, RedisPool, Channel,
+ create_pool, RedisPool, ConnectionsPool, Channel,
  RedisError, ProtocolError, ReplyError,
  PipelineError, MultiExecError, ConnectionClosedError,
  ChannelClosedError, WatchVariableError,
